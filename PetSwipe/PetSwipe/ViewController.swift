512aa8e7c329bd4dab9805996d2cd22f1e7f32f1
George Lee <george10lee@gmail.com>
2025-05-19 18:09:39 -0700

    create petswipe Project

---
+++ PetSwipe/PetSwipe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PetSwipe
+//
+//  Created by George Lee on 5/19/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PetSwipe/PetSwipeTests/PetSwipeTests.swift
@@ -0,0 +1,17 @@
+//
+//  PetSwipeTests.swift
+//  PetSwipeTests
+//
+//  Created by George Lee on 5/19/25.
+//
+
+import Testing
+@testable import PetSwipe
+
+struct PetSwipeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
