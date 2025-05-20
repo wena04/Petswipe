ab815d8bd2ac5163a167996aeda72cbb4623e1b1
George Lee <george10lee@gmail.com>
2025-05-19 19:07:09 -0700

    addPagesandMatchingFiles

---
+++ PetSwipe/PetSwipe/Pet.swift
@@ -0,0 +1,12 @@
+//
+//  Pet.swift
+//  PetSwipe
+//
+//  Created by George Lee on 5/19/25.
+// Class defining a single pet, and any associated functions/data we will need for it
+
+import Foundation
+
+class Pet{
+    
+}
---
+++ PetSwipe/PetSwipe/Scenes/MatchesPage.swift
@@ -0,0 +1,29 @@
+//
+//  MatchesPage.swift
+//  PetSwipe
+//
+//  Created by George Lee on 5/19/25.
+//
+
+import UIKit
+
+class MatchesPage: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
